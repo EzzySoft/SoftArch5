6374de10f3b2100af45326bc128088524ab81652
Julia Martynova <j.martynova@innopolis.university>
2024-10-09 17:57:16 +0300

    Changed registration and login handles

--- UserService/user_service.py
+++ UserService/user_service.py
@@ -8,18 +8,22 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique = True, nullable=False)
+    password = db.Column(db.String(50),  nullable=False)
 
 
 @app.route('/register', methods = ['POST'])
 def register():
     data = request.json
-    username = data.get("username")
-    password = data.get("password")
-    if User.query.filter_by(username = username).first():
+    username = data.get('username')
+    password = data.get('password')
+    if User.query.filter_by(username=username).first():
         return jsonify({
             "Message": "User already exists"
+        }), 400
+    if not username or not password:
+        return jsonify({
+            "Message": "Username and password are required"
         }), 400
     password = hashpw(password.encode('utf-8'), gensalt())
     user = User(user=username, password=password.decode('utf-8'))
@@ -34,9 +38,13 @@ def register():
 @app.route('/login', methods = ['POST'])
 def login():
     data = request.json
-    username = data.get['username']
-    password = data.get['password']
-    user = User.query.filter_by(username = username)
+    username = data.get('username')
+    password = data.get('password')
+    user = User.query.filter_by(username=username)
+    if not username or not password:
+        return jsonify({
+            "Message": "Username and password are required"
+        }), 400
     if not user:
         return jsonify({
             "Message": "User does not exist, please register"
@@ -44,7 +52,7 @@ def login():
     if not checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         return jsonify({
             "Message": "Passwords don't match. Please try again"
-        }),401
+        }), 401
     return jsonify(
         {
             "Message": "Successfull login"
