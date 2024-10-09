52acb4db44c93cdb88bf3390dba682fb66215e52
Julia Martynova <j.martynova@innopolis.university>
2024-10-10 00:28:50 +0300

    Updated User Service: added token generation

--- UserService/user_service.py
+++ UserService/user_service.py
@@ -1,66 +1,76 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from bcrypt import hashpw, gensalt, checkpw
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kmmrwsgr:UMg6sKjYUG3nhI6D3fTjSU3vMpjGjYCI@abul.db.elephantsql.com/kmmrwsgr'
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique = True, nullable=False)
-    password = db.Column(db.String(50),  nullable=False)
+    username = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(512), nullable=False)
 
 
-@app.route('/register', methods = ['POST'])
+@app.route('/register', methods=['POST'])
 def register():
     data = request.json
     username = data.get('username')
     password = data.get('password')
+
     if User.query.filter_by(username=username).first():
         return jsonify({
             "Message": "User already exists"
         }), 400
+
     if not username or not password:
         return jsonify({
             "Message": "Username and password are required"
         }), 400
+
     password = hashpw(password.encode('utf-8'), gensalt())
     user = User(username=username, password=password.decode('utf-8'))
     db.session.add(user)
     db.session.commit()
 
     return jsonify({
-        "Message" : "User registered successfully"
+        "Message": "User registered successfully"
     }), 201
 
 
-@app.route('/login', methods = ['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-    user = User.query.filter_by(username=username)
+
     if not username or not password:
         return jsonify({
             "Message": "Username and password are required"
         }), 400
+
+    user = User.query.filter_by(username=username).first()
     if not user:
         return jsonify({
             "Message": "User does not exist, please register"
         }), 401
+
     if not checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         return jsonify({
             "Message": "Passwords don't match. Please try again"
         }), 401
-    return jsonify(
-        {
-            "Message": "Successfull login"
-        }
-    ), 200
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({
+        "access_token": access_token
+    }), 200
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(port=5001)
+
